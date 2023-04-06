94ef6951324c252bb0f6915b642c58d4febe7f70
mohsinghani <mohsinghani.777@gmail.com>
2023-04-06 17:30:45 +0500

    complate header

---
+++ public/assets/images/index.js
@@ -0,0 +1,37 @@
+import milto from "./milto.png";
+import dragon from "./dragon.png";
+import Roblox from "./Roblox.png";
+import Vector from "./Vector.png";
+import logo from "./logo.png";
+import twiter from "./twiter.png";
+import coin from "./coin.svg";
+import union from "./union.png";
+import subtract from "./subtract.png";
+import vector3 from "./vector3.png";
+import vector4 from "./vector4.svg";
+import union2 from "./union2.png";
+import coin2 from "./coin2.png";
+import rolboxpapa from "./rolboxPapa.png";
+import cambro from "./cambro.png";
+import Vector25 from "./Vector25.svg";
+import Vector27 from "./Vector27.svg";
+
+export default {
+  milto,
+  dragon,
+  Roblox,
+  Vector,
+  logo,
+  twiter,
+  coin,
+  union,
+  subtract,
+  vector3,
+  union2,
+  rolboxpapa,
+  cambro,
+  vector4,
+  coin2,
+  Vector25,
+  Vector27,
+};
